472f9b377d7e7f7a557218f1e4c03f9544fb7c61
Your Name <srinathbala00@gmail.com>
2023-04-06 15:19:21 +0530

    Initial commit

---
+++ test/docker-zap.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DockerZap from '../lib/docker-zap-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/docker-zap-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DockerZap.DockerZapStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
