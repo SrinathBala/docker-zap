14055b0389de4c6bbd7d84252d8ab2438b774629
Your Name <srinathbala00@gmail.com>
2023-04-06 15:36:56 +0530

    commit message

--- lib/docker-zap-stack.ts
+++ lib/docker-zap-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DockerZapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+    });
+ 
+    
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc,
+      containerInsights: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DockerZapQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a task definition for your container
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'MyTaskDefinition', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+
+    // Add a container to the task definition
+    const container = taskDefinition.addContainer('MyContainer', {
+      image: ecs.ContainerImage.fromRegistry('demo-docker/flask_image'),
+      memoryReservationMiB: 256,
+      logging: ecs.LogDriver.awsLogs({ streamPrefix: 'my-container-logs' }),
+    });
+
+    // Create a service for the container
+    const service = new ecs.FargateService(this, 'MyService', {
+      cluster: cluster,
+      taskDefinition: taskDefinition,
+      desiredCount: 1,
+      serviceName: 'my-service-name',
+      assignPublicIp: true,
+    });
   }
 }
