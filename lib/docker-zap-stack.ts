69a1d1f1e68a4d8910de25a9154e05006c27a37f
Your Name <srinathbala00@gmail.com>
2023-04-09 22:27:28 +0530

    added workflow

--- lib/docker-zap-stack.ts
+++ lib/docker-zap-stack.ts
@@ -55,6 +55,7 @@ export class DockerZapStack extends cdk.Stack {
       allowAllOutbound: true,
     });
     
+    
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow HTTP traffic');
     
     // Create a service for the container
